5399de23153f76e56c3f85b7f6fa586873aa6cde
jagannadhkalla <jagannadhkalla@gmail.com>
2024-09-25 16:23:17 +0530

    asdfghjk

--- driver-management/db.js
+++ driver-management/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Create connection to the MySQL database
 const db = mysql.createConnection({
-    host: 'localhost',    // Replace with your DB host
-    user: 'root',         // Replace with your DB user
-    password: 'king1504',         // Replace with your DB password
-    database: 'vemsFinal'  // Replace with your DB name
+    host: process.env.DB_HOST,    // Replace with your DB host
+    user: process.env.DB_USER,         // Replace with your DB user
+    password: process.env.DB_PASSWORD,         // Replace with your DB password
+    database: process.env.DATABASE  // Replace with your DB name
 }).promise();
 
 // Connect to the database
