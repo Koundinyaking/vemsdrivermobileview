926898022b871f3bab7da431ef88f29d6a3d0e60
Koundinyaking <koundinyaking504@gmail.com>
2024-09-26 11:55:59 +0530

    sad

--- driver-management/db.js
+++ driver-management/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,    // Replace with your DB host
     user: process.env.DB_USER,         // Replace with your DB user
     password: process.env.DB_PASSWORD,         // Replace with your DB password
-    database: process.env.DATABASE  // Replace with your DB name
+    database: process.env.DB_DATABASE  // Replace with your DB name
 }).promise();
 
 // Connect to the database
